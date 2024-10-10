2fb1cbb2c2a0a4f39e6512184f79294ae7e71dea
玉垒浮云 <yuleifuyunn@gmail.com>
2024-10-10 20:29:32 +0800

    Update ProteusDefinitions.swift

--- Proteus/Proteus/ProteusDefinitions.swift
+++ Proteus/Proteus/ProteusDefinitions.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 public extension Proteus {
-    struct ItemDimension {
+    struct CardDimension {
         enum Option {
             case fractionalWidth(CGFloat, padding: CGFloat)
             case fractionalHeight(CGFloat, padding: CGFloat)
@@ -21,24 +21,24 @@ public extension Proteus {
             self.option = option
         }
         
-        public static func fractionalWidth(_ fractionalWidth: CGFloat, padding: CGFloat = 0) -> ItemDimension {
-            ItemDimension(option: .fractionalWidth(fractionalWidth, padding: padding))
+        public static func fractionalWidth(_ fractionalWidth: CGFloat, padding: CGFloat = 0) -> CardDimension {
+            CardDimension(option: .fractionalWidth(fractionalWidth, padding: padding))
         }
 
-        public static func fractionalHeight(_ fractionalHeight: CGFloat, padding: CGFloat = 0) -> ItemDimension {
-            ItemDimension(option: .fractionalHeight(fractionalHeight, padding: padding))
+        public static func fractionalHeight(_ fractionalHeight: CGFloat, padding: CGFloat = 0) -> CardDimension {
+            CardDimension(option: .fractionalHeight(fractionalHeight, padding: padding))
         }
 
-        public static func absolute(_ absoluteDimension: CGFloat) -> ItemDimension {
-            ItemDimension(option: .absolute(absoluteDimension))
+        public static func absolute(_ absoluteDimension: CGFloat) -> CardDimension {
+            CardDimension(option: .absolute(absoluteDimension))
         }
     }
 
-    struct ItemSize {
-        public var width: ItemDimension
-        public var height: ItemDimension
+    struct CardSize {
+        public var width: CardDimension
+        public var height: CardDimension
         
-        public init(width: ItemDimension = .fractionalWidth(1), height: ItemDimension = .fractionalHeight(1)) {
+        public init(width: CardDimension = .fractionalWidth(1), height: CardDimension = .fractionalHeight(1)) {
             self.width = width
             self.height = height
         }
@@ -68,30 +68,16 @@ public extension Proteus {
             return CGSize(width: resolvedWidth, height: resolvedHeight)
         }
     }
-    
-    struct ItemTransform {
-        enum Option {
-            case none
-            case zoom(maxScale: CGFloat, anchorPoint: CGPoint, inactiveItemAlpha: CGFloat)
-            case custom((_ attributes: UICollectionViewLayoutAttributes) -> Void)
-        }
+
+    enum ScrollDirection: Int {
+        case leftToRight = 1
+        case rightToLeft = 2
+        case topToBottom = 3
+        case bottomToTop = 4
         
-        var option: Option
-        
-        init(option: Option) {
-            self.option = option
-        }
-        
-        public static let none = ItemTransform(option: .none)
-        
-        public static let defaultZoom = ItemTransform(option: .zoom(maxScale: 1.4, anchorPoint: .init(x: 0.5, y: 0.5), inactiveItemAlpha: 1))
-        
-        public static func zoom(maxScale: CGFloat, anchorPoint: CGPoint = .init(x: 0.5, y: 0.5), inactiveItemAlpha: CGFloat = 1) -> ItemTransform {
-            .init(option: .zoom(maxScale: maxScale, anchorPoint: anchorPoint, inactiveItemAlpha: inactiveItemAlpha))
-        }
-        
-        public static func custom(_ transform: @escaping (_ attributes: UICollectionViewLayoutAttributes) -> Void) -> ItemTransform {
-            .init(option: .custom(transform))
+        var isHorizontal: Bool {
+            return self == .leftToRight || self == .rightToLeft
         }
     }
 }
+
